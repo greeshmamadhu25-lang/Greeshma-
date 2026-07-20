2e365948bc0bc8078a9f3ec5dc969329ae0845f3
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-07-20 13:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Scroll Animation
 const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver((entries) => {
@@ -11,3 +12,59 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((section) => {
     observer.observe(section);
 });
+
+// Typing Effect
+
+const text = [
+    "B.Voc Information Technology Graduate",
+    "Web Developer",
+    "Flutter Developer",
+    "Front-End Enthusiast"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function type() {
+
+    if (charIndex < text[textIndex].length) {
+
+        typing.textContent += text[textIndex].charAt(charIndex);
+        charIndex++;
+
+        setTimeout(type, 80);
+
+    } else {
+
+        setTimeout(erase, 1800);
+
+    }
+
+}
+
+function erase() {
+
+    if (charIndex > 0) {
+
+        typing.textContent = text[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+
+        setTimeout(erase, 40);
+
+    } else {
+
+        textIndex++;
+
+        if (textIndex >= text.length) {
+            textIndex = 0;
+        }
+
+        setTimeout(type, 300);
+
+    }
+
+}
+
+type();
